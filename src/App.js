65f88ae3a9d16be3b5b800183857b710571bd677
Neil3108 <prakharsharma3108@gmail.com>
2023-06-19 19:16:45 -0500

    Final edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
+import EditUser from './features/users/EditUser'
+import NewUserForm from './features/users/NewUserForm'
+import EditNote from './features/notes/EditNote'
+import NewNote from './features/notes/NewNote'
+import Prefetch from './features/auth/Prefetch'
+import PersistLogin from './features/auth/PersistLogin'
+import RequireAuth from './features/auth/RequireAuth'
+import { ROLES } from './config/roles';
+import useTitle from './hooks/useTitle';
 
 function App() {
+  useTitle('Winnipeg Auto Repair Shop')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<Public />} />  {/* Home page (Public)*/}
+        <Route path="login" element={<Login />} /> {/* Login page (Public)*/}
+
+        {/* Start of Private routes*/}
+        <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]}/>}>
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}> {/* Dash page (Private)*/}
+
+                <Route index element={<Welcome />} /> {/* Dash page (Private)*/}
+                <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]}/>}>
+                  <Route path="users">
+                    <Route index element={<UsersList />} /> {/* User list page (Private)*/}
+                    <Route path=":id" element={<EditUser />} /> {/* Edit user page (Private)*/}
+                    <Route path="new" element={<NewUserForm />} />  {/* New user page (Private)*/}
+                  </Route>
+                </Route>
+
+                <Route path="notes">
+                  <Route index element={<NotesList />} /> {/* Notes list page (Private)*/}
+                  <Route path=":id" element={<EditNote />} /> {/* Edit note page (Private)*/}
+                  <Route path="new" element={<NewNote />} /> {/* New note page (Private)*/}
+                </Route>
+
+              </Route>{/* End Dash */}
+            </Route>
+          </Route>
+        </Route> 
+        {/* End of Private routes*/}
+      </Route>
+    </Routes>
   );
 }
 
